3ded3c3b2198c26aeb3ac83667b8eae07e2a4bac
AurelienChem <137295300+AurelienChem@users.noreply.github.com>
2023-06-28 16:52:02 +0200

    ajout d après 4/3 pour eviter la troncature

--- Algo-Java/Cercle/src/cercle/Cercle.java
+++ Algo-Java/Cercle/src/cercle/Cercle.java
@@ -20,7 +20,7 @@ public class Cercle {
 		if(an == 0) {
 			
 			a = 4*Math.PI*Math.pow(r, 2);
-			v = (4/3)*Math.PI*Math.pow(r, 3);
+			v = (4/3d)*Math.PI*Math.pow(r, 3);
 			
 			System.out.println("L'aire vaut " + a + " et le volume vaut " + v);
 		}
@@ -28,10 +28,10 @@ public class Cercle {
 		else {
 			
 			a = 4*Math.PI*Math.pow(r, 2);
-			v = (4/3)*Math.PI*Math.pow(r, 3);
+			v = (4/3d)*Math.PI*Math.pow(r, 3); // le d après 4/3 permet de dire que 4/3 ne vaut pas 1 en troncature mais 1.33
 			s = Math.PI*Math.pow(r, 2)*an/360;
 					
-			System.out.println("L'aire vaut " + a + ", le volume vaut " + v + " et l’aire du secteur circulaire vaut " + s);
+			System.out.println("L'aire vaut " + a + ", le volume vaut " + v + " et l'aire du secteur circulaire vaut " + s);
 			
 			
 		scan.close();
