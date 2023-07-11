a5f30c1a666b68992e8e90f59bd8a19a14da75f7
GIBIER Aurélien <AGibier@arfp.asso.fr>
2023-07-11 16:46:58 +0200

    JAVA caractères

--- Algo-Java/Swap/src/tableau2/App.java
+++ Algo-Java/Swap/src/tableau2/App.java
@@ -8,19 +8,27 @@ public class App {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		String[] tableau = new String[] {"appartement."};
-		String caracLettres;
-		boolean presenceLettre;
+		String motTest = " ";
+		String lettreTest = " ";
+		var position = (motTest.indexOf(lettreTest));
+		position = 0;
+		
+		System.out.println("Entrez un mot");
+		motTest = sc.next();
 		
 		System.out.println("Entrez une lettre");
-		caracLettres = sc.next();
+		lettreTest = sc.next();
 		
-		if(caracLettres.equals(tableau)) {
-			
-			
+		for(int i=0; i < motTest.length(); i++) {
+		
+			if(motTest.indexOf(lettreTest) == i) {
+				System.out.println(motTest.indexOf(lettreTest) +" est present dans le mot");
+					position++;
+				 }
+			else {
+				System.out.println(motTest.indexOf(lettreTest) + " est pas present dans le mot");
+				 }
 		}
-		
-		
 		sc.close();
 
 	}
