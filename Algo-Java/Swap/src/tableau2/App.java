7581e9d008cc75d42e4dbdcd2993ad756cc8563f
GIBIER Aurélien <AGibier@arfp.asso.fr>
2023-07-12 16:11:16 +0200

    Exercice 3.4 caracteres

--- Algo-Java/Swap/src/tableau2/App.java
+++ Algo-Java/Swap/src/tableau2/App.java
@@ -10,8 +10,6 @@ public class App {
 		
 		String motTest = " ";
 		String lettreTest = " ";
-		var position = (motTest.indexOf(lettreTest));
-		position = 0;
 		
 		System.out.println("Entrez un mot");
 		motTest = sc.next();
@@ -19,18 +17,17 @@ public class App {
 		System.out.println("Entrez une lettre");
 		lettreTest = sc.next();
 		
-		for(int i=0; i < motTest.length(); i++) {
-		
-			if(motTest.indexOf(lettreTest) == i) {
-				System.out.println(motTest.indexOf(lettreTest) +" est present dans le mot");
-					position++;
-				 }
-			else {
-				System.out.println(motTest.indexOf(lettreTest) + " est pas present dans le mot");
-				 }
+		if(motTest.equals(" ") || motTest.equals(".")) {
+			System.out.println("La phrase est vide");
+		}
+		else if (motTest.indexOf(lettreTest) != -1) {
+			for(int i = motTest.indexOf(lettreTest); i >= 0;  i = motTest.indexOf(lettreTest, i + 1)) {
+				System.out.println(lettreTest  + " est present dans le mot en position " + i);
+			}
+		}
+		else {
+			System.out.println(lettreTest +  " n'est pas present dans le mot");
 		}
 		sc.close();
-
 	}
-
 }
