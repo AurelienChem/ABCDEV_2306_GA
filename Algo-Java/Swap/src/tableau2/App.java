e4ad2b1c183c70625abe2ca567d3e3fe7019da5b
GIBIER Aurélien <AGibier@arfp.asso.fr>
2023-07-13 09:25:52 +0200

    Modification exercices 3.4

--- Algo-Java/Swap/src/tableau2/App.java
+++ Algo-Java/Swap/src/tableau2/App.java
@@ -12,17 +12,17 @@ public class App {
 		String lettreTest = " ";
 		
 		System.out.println("Entrez un mot");
-		motTest = sc.next();
+		motTest = sc.nextLine() + ".";
 		
 		System.out.println("Entrez une lettre");
 		lettreTest = sc.next();
 		
-		if(motTest.equals(" ") || motTest.equals(".")) {
+		if(motTest.equals(".") || motTest.equals("..") ) {
 			System.out.println("La phrase est vide");
 		}
 		else if (motTest.indexOf(lettreTest) != -1) {
 			for(int i = motTest.indexOf(lettreTest); i >= 0;  i = motTest.indexOf(lettreTest, i + 1)) {
-				System.out.println(lettreTest  + " est present dans le mot en position " + i);
+				System.out.println(lettreTest  + " est present dans le mot en position " + (i + 1));
 			}
 		}
 		else {
