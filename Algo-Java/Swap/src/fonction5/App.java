c4fc7b072c0d00b4950a7a2da2a9e468e674dc59
GIBIER Aurélien <AGibier@arfp.asso.fr>
2023-08-01 08:24:40 +0200

    Exercies procedure et fonction

--- Algo-Java/Swap/src/fonction5/App.java
+++ Algo-Java/Swap/src/fonction5/App.java
@@ -30,28 +30,30 @@ public class App {
 	private static String jourdelasemaine(int a, int b, int c) {
 		String resultat = "";
 		
-		if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 0 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 0) {
+		// https://fr.wikibooks.org/wiki/Curiosit%C3%A9s_math%C3%A9matiques/Trouver_le_jour_de_la_semaine_avec_une_date_donn%C3%A9e#M%C3%A9thode_1
+		
+			if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 0 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 0) {
 			System.out.println("le jour est un dimanche");
-		}
-		else if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 1 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 1) {
+			}
+			else if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 1 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 1) {
 			System.out.println("le jour est un lundi");
-		}
-		else if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 2 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 2) {
+			}
+			else if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 2 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 2) {
 			System.out.println("le jour est un mardi");
-		}
-		else if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 3 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 3) {
+			}
+			else if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 3 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 3) {
 			System.out.println("le jour est un mercredi");
-		}
-		else if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 4 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 4) {
+			}
+			else if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 4 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 4) {
 			System.out.println("le jour est un jeudi");
-		}
-		else if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 5 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 5) {
+			}
+			else if(b < 3 && (Math.floor(23*b/9d) + a + 4 + c + Math.floor((c-1)/4d) - Math.floor((c-1)/100d) + Math.floor((c-1)/400d))%7 == 5 || b >= 3 && (Math.floor(23*b/9d) + a + 2 + c + Math.floor(c/4d) - Math.floor(c/100d) + Math.floor(c/400d))%7 == 5) {
 			System.out.println("le jour est un vendredi");
-		}
-		else {
+			}
+			else {
 			System.out.println("le jour est un samedi");
-		}		
-		return resultat;
+			}		
+			return resultat;
 	}
 }
 
--- Algo-Java/Swap/src/tableau1_2/App.java
+++ Algo-Java/Swap/src/tableau1_2/App.java
@@ -33,7 +33,6 @@ public class App {
         {
             System.out.print(tableauvoyelle[i] + " ");
         }
-   
 
         sc.close();
 	}
