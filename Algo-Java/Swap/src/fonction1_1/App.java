a0e1b5062bafc49989c7ea0dfd5950444326565d
GIBIER Aurélien <AGibier@arfp.asso.fr>
2023-08-02 10:05:15 +0200

    Ajout pseudo-code + organigramme fonction1_1

--- Algo-Java/Swap/src/fonction1_1/App.java
+++ Algo-Java/Swap/src/fonction1_1/App.java
@@ -7,6 +7,51 @@ public class App {
 	public static void main(String[] args)
 	{
 
+		/*
+		 FONCTION
+		 	entier ordre(entier a, entier b, entier c) 
+		 	resultatOrdre est un entier
+		 	resultatOrdre <-- a+b+c
+		 	retourne resultatOrdre
+		 fin fonction
+		 FONCTION
+		 	reel CB(reel a, reel b, reel c) 
+		 	resultatCB est un reel
+		 	resultatCB <-- a/(a + b + c)*100
+		 	retourne resultatCB
+		 fin fonction
+		 FONCTION
+		 	reel cheque(reel a, reel b, reel c) 
+		 	resultatCheque est un reel
+		 	resultatCheque <-- b/(a + b + c)*100
+		 	retourne resultatCheque
+		 fin fonction
+		 FONCTION
+		 	reel virement(reel a, reel b, reel c) 
+		 	resultatVirement est un reel
+		 	resultatVirement <-- c/(a + b + c)*100
+		 	retourne resultatVirement
+		 fin fonction
+		 Variables
+		 nbCB, nbCheque, nbVirement sont des reels
+		 Debut du programme
+		 Ecrire "Entrez le nombre de paiement par carte bleue : "
+		 Lire nbCB
+		 Ecrire "Entrez le nombre de cheque emis : "
+		 Lire nbCheque
+		 Ecrire "Entrez le nombre de virement automatique : ")
+		 Lire nbVirement
+		 Ecrire "Nombre de paiements par Carte Bleue est : " nbCB
+		 Ecrire "Nombre de cheques emis : " nbCheque
+		 Ecrire "Nombre de virements automatiques : " + nbVirement
+		 Ecrire "Vous avez emis " ordre(nbCB, nbCheque, nbVirement) " ordres de debit dont :"
+		 Ecrire CB(nbCB, nbCheque, nbVirement) + " % par Carte Bleue"
+		 Ecrire cheque(nbCB, nbCheque, nbVirement) + " % par cheque"
+		 Ecrire virement(nbCB, nbCheque, nbVirement) + " % virement"
+		 Fin de programme
+		 
+		 */
+		
 	Scanner sc = new Scanner(System.in);
 	
 		int nbCB;
