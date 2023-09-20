a1e72b686439a34e3e2b844903aec8c099aecc36
GIBIER Aurélien <AGibier@arfp.asso.fr>
2023-09-20 15:32:53 +0200

    Heritage AnimalAbeilleApp

---
+++ Algo-Java/Swap/src/animalHeritage/App.java
@@ -0,0 +1,13 @@
+package animalHeritage;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Abeille abeille1 = new Abeille("Apide", "bourdonne", "volant", "nectar", "abeille");
+		
+		abeille1.afficherTout();
+		
+	}
+
+}
