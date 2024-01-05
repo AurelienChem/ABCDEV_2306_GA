1516c6bd2dc0fc4cbb8dbeefa8041738bad0b77d
GIBIER Aurélien <AGibier@arfp.asso.fr>
2024-01-05 09:04:05 +0100

    Nombre Premier + Test

---
+++ CSharp/Nombre Premier/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace NombrePremier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre;
+            string saisie;
+
+            Console.WriteLine("Saisir un nombre");
+
+            saisie = Console.ReadLine();
+            nombre = int.Parse(saisie);
+
+            Console.WriteLine("nombre " + nombre + " increment " + Type(nombre));
+
+        }
+
+        public static int Type(int nombre) {
+
+            int i;
+            int j = 0;
+
+            if (nombre <= 1)
+            {
+                j++;
+            }
+            else
+            {
+
+                for (i = 2; i <= nombre - 1; i++)
+                {
+                    if (nombre % i == 0)
+                    {
+                        j++;
+
+                    }
+                }
+            }
+
+            return j;
+        }
+    }
+}
