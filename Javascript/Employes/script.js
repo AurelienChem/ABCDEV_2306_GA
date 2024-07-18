59f88bd514a9d62eeb477b9c56a908ad79041923
AurelienChem <AurelienChem@users.noreply.github.com>
2024-07-18 16:16:36 +0200

    TP employes WIP

---
+++ Javascript/Employes/script.js
@@ -0,0 +1,106 @@
+let response = await fetch('./employees.json');
+let datas = await response.json();
+
+let i = 1;
+for (const element of datas.data) {
+
+    let row = document.createElement('tr');
+
+
+    row.setAttribute('id', 'p' + i) 
+
+    let monItem1 = document.createElement("td");
+    let monItem2 = document.createElement("td");
+    let monItem3 = document.createElement("td");
+    let monItem4 = document.createElement("td");
+    let monItem5 = document.createElement("td");
+    let monItem6 = document.createElement("td");
+
+    let objet = document.getElementById("objet2");
+
+    objet.appendChild(monItem1).textContent = element.id;
+    objet.appendChild(monItem2).textContent = element.employee_name;
+
+    let lemail = element.employee_name.split(' ');
+    objet.appendChild(monItem3).textContent = (lemail[0][0] + "." + lemail[1] + "@email.com").toLowerCase()
+
+    objet.appendChild(monItem4).textContent =  (element.employee_salary/12).toFixed(2) + " €";
+    objet.appendChild(monItem5).textContent = 2024 - element.employee_age;
+
+    let buttonDup = document.createElement('button');
+    let buttonSup = document.createElement('button');
+    buttonDup.textContent = 'Dupliquer';
+    buttonSup.textContent = 'Supprimer';
+    objet.appendChild(monItem6).appendChild(buttonDup).setAttribute('id', 'd' + i);
+    objet.appendChild(monItem6).appendChild(buttonSup).setAttribute('id', 's' + i);
+
+    i++;
+
+    row.append(monItem1, monItem2, monItem3, monItem4, monItem5, monItem6);
+    document.getElementById("objet2").appendChild(row);
+}
+
+function callNewbutton() {
+for(let n = 0; n < datas.data.length + 1; n++) {
+const btnDup = document.querySelectorAll("#d" + n);
+const btnSup = document.querySelectorAll("#s" + n);
+
+btnDup.forEach((btn) => {
+    btn.addEventListener("click", duplicate);
+    console.log(btnDup)
+});
+}
+}
+
+callNewbutton();
+
+let j = 25;
+
+function duplicate() { {
+
+for(let k = 0; k < datas.data.length; k++) {
+
+if(parseInt(this.id.replace('d','')) === (k + 1)) {
+
+    let person = structuredClone(datas.data[k])
+    datas.data.push(person)
+    
+    let row = document.createElement('tr');
+
+    row.setAttribute('id', 'p' + i) 
+
+    let monItem1 = document.createElement("td");
+    let monItem2 = document.createElement("td");
+    let monItem3 = document.createElement("td");
+    let monItem4 = document.createElement("td");
+    let monItem5 = document.createElement("td");
+    let monItem6 = document.createElement("td");
+
+    let objet = document.getElementById("objet2");
+
+    objet.appendChild(monItem1).textContent = j++;
+    objet.appendChild(monItem2).textContent = person.employee_name;
+
+    let lemail = person.employee_name.split(' ');
+    objet.appendChild(monItem3).textContent = (lemail[0][0] + "." + lemail[1] + "@email.com").toLowerCase()
+
+    objet.appendChild(monItem4).textContent =  (person.employee_salary/12).toFixed(2) + " €";
+    objet.appendChild(monItem5).textContent = 2024 - person.employee_age;
+
+    let buttonDup = document.createElement('button');
+    let buttonSup = document.createElement('button');
+    buttonDup.textContent = 'Dupliquer';
+    buttonSup.textContent = 'Supprimer';
+    objet.appendChild(monItem6).appendChild(buttonDup).setAttribute('id', 'd' + i);
+    objet.appendChild(monItem6).appendChild(buttonSup).setAttribute('id', 's' + i);
+
+    i++;
+
+    row.append(monItem1, monItem2, monItem3, monItem4, monItem5, monItem6);
+    document.getElementById("objet2").appendChild(row);
+
+    callNewbutton();
+}
+}
+}
+}
