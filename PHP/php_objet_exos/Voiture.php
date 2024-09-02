028b06ea7d0621afe3570ebb4beb659364ab8947
AurelienChem <AurelienChem@users.noreply.github.com>
2024-09-02 16:50:19 +0200

    Try/Catch Voiture

--- PHP/php_objet_exos/Voiture.php
+++ PHP/php_objet_exos/Voiture.php
@@ -8,25 +8,33 @@ class Voiture
 	protected $poids = 1000;
 
 	public function __construct(string $marque, string $modele, int $poids) {
-		$this->marque = $marque;
-		$this->modele = $modele;
+		if(mb_strlen($marque) > 2) {
+			$this->marque = $marque;
+		}
+		else {
+			throw new Exception('Erreur, la marque indiqué possède moins de deux caractères');
+		}
+
+		if(mb_strlen($modele) > 1) {
+			$this->modele = $modele;
+		}
+		else {
+			throw new Exception('Erreur, il n y a pas de modèle indiqué');
+		}
+
+		if($poids > 0) {
+			$this->poids = $poids;
+		}
+		else {
+			throw new Exception('Le poids ne peut pas être inférieur ou égal à zéro');
+		}
+
 		$this->poids = $poids;
 	}
 
 	public function __toString(): string
 	{
-		if(mb_strlen($this->getMarque()) < 2 ) {
-			return 'Veuillez saisir une marque avec plus de un caractère';
-		}
-		elseif(mb_strlen($this->getModele()) < 1 ) {
-			return 'Veuillez saisir un modèle';
-		}
-		else if($this->getPoids() <= 0 && preg_match('/[0-9]/', $this->getPoids())) {
-			return 'Veuillez saisir un poids en nombre supérieur à 0';
-		}
-		else {
-			return "{$this->getMarque()} {$this->getModele()}, {$this->getPoids()} Kg";
-		}
+		return "{$this->getMarque()} {$this->getModele()}, {$this->getPoids()} Kg";
 	}
 
 	public function getMarque(): string
@@ -61,6 +69,12 @@ class Voiture
 	}
 }
 
-$myVoiture = new Voiture('TOYOTA', 'Sprinter', 0);
-echo $myVoiture;
+try {
+	$myVoiture = new Voiture('Toyota', 'Sprinter', 900);
+	echo $myVoiture;
+}
+
+catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
 
